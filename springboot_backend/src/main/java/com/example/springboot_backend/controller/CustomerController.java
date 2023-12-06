cbac6e110daf2ed603fc58daf8ebfef082a496ef
sehan22 <sehanranaweera2002@gmail.com>
2023-12-06 15:37:10 +0530

    create frontend project with angular & backend project with springboot

---
+++ springboot_backend/src/main/java/com/example/springboot_backend/controller/CustomerController.java
@@ -0,0 +1,23 @@
+package com.example.springboot_backend.controller;
+
+import com.example.springboot_backend.model.Customer;
+import com.example.springboot_backend.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class CustomerController {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @GetMapping("/customer")
+    public List<Customer> getAllCustomers() {
+        System.out.println(customerRepository.findAll());
+        return customerRepository.findAll();
+    }
+}
---
+++ springboot_backend/src/main/java/com/example/springboot_backend/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.example.springboot_backend.repository;
+
+import com.example.springboot_backend.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
